86d621b7e0c60e52fba1af43daada5fcebcf7d4d
jonathan simonney <jonathan.simonney@polyconseil.fr>
2019-12-20 16:02:47 +0100

    now working as expected

--- app/src/main/java/com/example/androidformation/IntentActivity.java
+++ app/src/main/java/com/example/androidformation/IntentActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class IntentActivity extends AppCompatActivity {
     public static void startActivity(Context context, String text){
@@ -13,9 +14,19 @@ public class IntentActivity extends AppCompatActivity {
         context.startActivity(intent);
     }
 
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent);
+
+        this.textView = findViewById(R.id.precedent_input_text);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras != null && extras.containsKey(Intent.EXTRA_TEXT)) {
+            String inputString = extras.getString(Intent.EXTRA_TEXT);
+            this.textView.setText(inputString);
+        }
     }
 }
